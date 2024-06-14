0a1804b2c8926c155a0cca025daffe069c6d0da2
kyrylo.m <kirill.anisimovv@gmail.com>
2024-06-14 14:44:55 +0300

    Add sample nextjs app with TRPC

---
+++ apps/frontpage/src/server/api/routers/post.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+let post = {
+  id: 1,
+  name: "Hello World",
+};
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
